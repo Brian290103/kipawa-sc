31fb92e00c14416a9bcec669ab0f80d7f76adcc3
Brian Saitabau <119662170+Brian290103@users.noreply.github.com>
2025-05-15 07:44:50 +0300

    Update app-sidebar.tsx
    removed embrace afrika text from the sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -159,7 +159,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Embrace Afrika</span>
+                <span className="text-base font-semibold">Kipawa SC</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
